d38d56160ea9a096b4924b9295c93ef90fd0cb03
Aayushman117 <aayushmansemwal117@gmail.com>
2025-07-15 10:08:56 +0530

    WAP to accept marks of 4 subjects, display its total makrs, Average and grade according for min 3 students

--- main.cpp
+++ main.cpp
@@ -32,28 +32,64 @@ using namespace std;
 //     return 0;
 // }
 
+//
+// struct subject {
+//     int marks;
+// };
+// int main() {
+//     subject s1, s2, s3, s4;
+//     cout << "Enter details of 1st subject: ";
+//     cin >> s1.marks;
+//     cout << "Enter details of 2nd subject: ";
+//     cin >> s2.marks;
+//     cout << "Enter details of 3rd subject: ";
+//     cin >> s3.marks;
+//     cout << "Enter details of 4th subject: ";
+//     cin >> s4.marks;
+//     int tm = s1.marks + s2.marks + s3.marks + s4.marks;  // ✅ correct summing of marks
+//     float avg = tm / 4.0;
+//     cout << "Total Marks: " << tm << endl;
+//     cout << "Average Marks: " << avg << endl;
+//     return 0;
+// }
 
-struct subject {
-    int marks;
+
+
+struct Student {
+    int marks[4];   // Marks in 4 subjects
+    int total;
+    float average;
+    char grade;
 };
+char calculateGrade(float avg) {
+    if (avg >= 90) return 'A';
+    else if (avg >= 75) return 'B';
+    else if (avg >= 60) return 'C';
+    else if (avg >= 40) return 'D';
+    else return 'F';
+}
 int main() {
-    subject s1, s2, s3, s4;
-    cout << "Enter details of 1st subject: ";
-    cin >> s1.marks;
-    cout << "Enter details of 2nd subject: ";
-    cin >> s2.marks;
-    cout << "Enter details of 3rd subject: ";
-    cin >> s3.marks;
-    cout << "Enter details of 4th subject: ";
-    cin >> s4.marks;
-    int tm = s1.marks + s2.marks + s3.marks + s4.marks;  // ✅ correct summing of marks
-    float avg = tm / 4.0;
-    cout << "Total Marks: " << tm << endl;
-    cout << "Average Marks: " << avg << endl;
+    Student s[3];  // For 3 students
+    for (int i = 0; i < 3; i++) {
+        cout << "Enter marks for 4 subjects of Student " << i + 1 << ":\n";
+        s[i].total = 0;
+        for (int j = 0; j < 4; j++) {
+            cout << "Subject " << j + 1 << ": ";
+            cin >> s[i].marks[j];
+            s[i].total += s[i].marks[j];
+        }
+        s[i].average = s[i].total / 4.0;
+        s[i].grade = calculateGrade(s[i].average);
+    }
+    cout<<endl;
+    for (int i = 0; i < 3; i++) {
+        cout << "Student " << i + 1 << ":\n";
+        cout << "Total Marks = " << s[i].total << endl;
+        cout << "Average = " << s[i].average << endl;
+        cout << "Grade = " << s[i].grade << endl << endl;
+    }
     return 0;
 }
-
-
 // TIP See CLion help at <a
 // href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
 //  Also, you can try interactive lessons for CLion by selecting
