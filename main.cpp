6ad4ed55f744b7e3fd8cf3d0b9e4c8b884e820f5
Aayushman117 <aayushmansemwal117@gmail.com>
2025-07-16 09:08:48 +0530

    WAP to accept marks of 4 subjects, display its total makrs, Average and grade according for min 3 students

--- main.cpp
+++ main.cpp
@@ -55,41 +55,122 @@ using namespace std;
 
 
 
-struct Student {
-    int marks[4];   // Marks in 4 subjects
-    int total;
-    float average;
-    char grade;
+// struct Student {
+//     int marks[4];   // Marks in 4 subjects
+//     int total;
+//     float average;
+//     char grade;
+// };
+// char calculateGrade(float avg) {
+//     if (avg >= 90) return 'A';
+//     else if (avg >= 75) return 'B';
+//     else if (avg >= 60) return 'C';
+//     else if (avg >= 40) return 'D';
+//     else return 'F';
+// }
+// int main() {
+//     Student s[3];  // For 3 students
+//     for (int i = 0; i < 3; i++) {
+//         cout << "Enter marks for 4 subjects of Student " << i + 1 << ":\n";
+//         s[i].total = 0;
+//         for (int j = 0; j < 4; j++) {
+//             cout << "Subject " << j + 1 << ": ";
+//             cin >> s[i].marks[j];
+//             s[i].total += s[i].marks[j];
+//         }
+//         s[i].average = s[i].total / 4.0;
+//         s[i].grade = calculateGrade(s[i].average);
+//     }
+//     cout<<endl;
+//     for (int i = 0; i < 3; i++) {
+//         cout << "Student " << i + 1 << ":\n";
+//         cout << "Total Marks = " << s[i].total << endl;
+//         cout << "Average = " << s[i].average << endl;
+//         cout << "Grade = " << s[i].grade << endl << endl;
+//     }
+//      return 0;
+// }
+
+
+
+
+
+// struct Student {
+//     int marks[4];   // Marks in 4 subjects
+//     int total;
+//     float average;
+//     char grade;
+// };
+// char calculateGrade(float avg) {
+//     if (avg >= 90) return 'A';
+//     else if (avg >= 75) return 'B';
+//     else if (avg >= 60) return 'C';
+//     else if (avg >= 40) return 'D';
+//     else return 'F';
+// }
+// int main() {
+//     Student s[3];  // For 3 students
+//     for (int i = 0; i < 3; i++) {
+//         cout << "Enter marks for 4 subjects of Student " << i + 1 << ":\n";
+//         s[i].total = 0;
+//         for (int j = 0; j < 4; j++) {
+//             cout << "Subject " << j + 1 << ": ";
+//             cin >> s[i].marks[j];
+//             s[i].total += s[i].marks[j];
+//         }
+//         s[i].average = s[i].total / 4.0;
+//         s[i].grade = calculateGrade(s[i].average);
+//     }
+//     cout<<endl;
+//     for (int i = 0; i < 3; i++) {
+//         cout << "Student " << i + 1 << ":\n";
+//         cout << "Total Marks = " << s[i].total << endl;
+//         cout << "Average = " << s[i].average << endl;
+//         cout << "Grade = " << s[i].grade << endl << endl;
+//     }
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+// class Box {
+// public:
+//     int length;
+//     int width;
+//     int area() {
+//         return length * width;
+//     }
+// };
+//
+// int main() {
+//     Box b1;
+//     b1.length = 5;
+//     b1.width =3;
+//     cout<<"Area:"<<b1.area()<<endl;
+//     return 0;
+// }
+
+class Person {
+public:
+    string name;
+    Person(string n) {
+        name = n;
+    }
 };
-char calculateGrade(float avg) {
-    if (avg >= 90) return 'A';
-    else if (avg >= 75) return 'B';
-    else if (avg >= 60) return 'C';
-    else if (avg >= 40) return 'D';
-    else return 'F';
-}
+
 int main() {
-    Student s[3];  // For 3 students
-    for (int i = 0; i < 3; i++) {
-        cout << "Enter marks for 4 subjects of Student " << i + 1 << ":\n";
-        s[i].total = 0;
-        for (int j = 0; j < 4; j++) {
-            cout << "Subject " << j + 1 << ": ";
-            cin >> s[i].marks[j];
-            s[i].total += s[i].marks[j];
-        }
-        s[i].average = s[i].total / 4.0;
-        s[i].grade = calculateGrade(s[i].average);
-    }
-    cout<<endl;
-    for (int i = 0; i < 3; i++) {
-        cout << "Student " << i + 1 << ":\n";
-        cout << "Total Marks = " << s[i].total << endl;
-        cout << "Average = " << s[i].average << endl;
-        cout << "Grade = " << s[i].grade << endl << endl;
-    }
+    Person p("Alice");
+    cout<<p.name<<endl;
     return 0;
 }
+
+
+
+
+
+
 // TIP See CLion help at <a
 // href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
 //  Also, you can try interactive lessons for CLion by selecting
